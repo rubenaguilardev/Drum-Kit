19b4b515c5f3f81af852b0809339f3798209c4d3
rubenaguilardev <ragui56@wgu.edu>
2025-01-02 21:53:26 -0800

    Add Javascript

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let activeButton = document.querySelectorAll(".drum");
 
 activeButton.forEach(button => {
     button.addEventListener('click', function () {
-        let audio = new Audio("sounds/tom-1.mp3")
-        audio.play()
+        let audio = new Audio(`sounds/${this.innerText}.mp3`);
+        audio.play();
+        
     })
 })
