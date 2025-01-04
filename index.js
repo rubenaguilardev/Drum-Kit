04037a85cb84bf24050246512540dc79f8b32dde
rubenaguilardev <ragui56@wgu.edu>
2025-01-04 00:10:40 -0800

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let buttons = document.querySelectorAll(".drum");
 
 buttons.forEach(button => {
-    button.addEventListener('click', function () {
+    button.addEventListener('touchend', function () {
         let buttonKey = this.innerText
         sound(buttonKey)
         buttonAnimation(buttonKey)
