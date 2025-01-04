af54a7b82a7bd7ed1b1cf7604db8121d455a55e1
rubenaguilardev <ragui56@wgu.edu>
2025-01-03 23:36:23 -0800

    Add button animation

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ let activeButton = document.querySelectorAll(".drum");
 activeButton.forEach(button => {
     document.addEventListener('keydown', function (event) {
         let keyPressed = event.key
-        let audio = new Audio(`sounds/${keyPressed}.mp3`);
-            audio.play();
+        let audio = new Audio(`sounds/${keyPressed}.mp3`)
+        audio.play();
+        buttonAnimation(keyPressed)
    })
 })
 
 
+function buttonAnimation(key) {
+    let activeButton = document.querySelector(`.${key}`)
+    activeButton.classList.add("pressed")
+    setTimeout(function () {
+        activeButton.classList.remove("pressed")
+    }, 100)
+    
+}
